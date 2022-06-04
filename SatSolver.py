34b557daa0acad89ba0aef27774f26b313ce2e10
Alexandre Laval <alexandre.laval@etu.utc.fr>
2022-06-04 19:48:04 +0200

    mise à jour de la map terminer, convertion en format cnf
    Signed-off-by: Alexandre Laval <alexandre.laval@etu.utc.fr>

--- SatSolver.py
+++ SatSolver.py
@@ -8,9 +8,45 @@ def successeur(Cases, action, case):
         return [(case[0], case[1] + 1)]
     return []
 
-#fonction qui permet de savoir ce que modifie une action, et peut donc être utilisé pour identifier les entités non modifiées
-def consequence(action, case):
-    return
+
+def get_nombre_variable(Clauses):
+    nb_variable = 0
+    vars = []
+    for clause in Clauses:
+        if type(clause) == list:
+            for var in clause:
+                if var not in vars:
+                    vars.append(var)
+                    nb_variable += 1
+        else:
+            if clause not in vars:
+                vars.append(clause)
+                nb_variable += 1
+    return nb_variable
+
+
+def creation_cnf(clauses):
+    text = "c === Helltaker SAT solver === \n"
+    text += 'c\n'
+    text += f'p cnf {len(clauses)} {get_nombre_variable(clauses)} \n'
+    for clause in clauses:
+        if type(clause) == list:
+            for var in clause:
+                text += f'{var} '
+            text += '0 \n'
+        else:
+            text += f'{clause} 0 \n'
+
+    return text
+
+
+def get_target(action):
+    if action in ["attaquerHaut", "attaquerBas", "attaquerGauche", "attaquerDroite"]:
+        return "M"
+    elif action in ["pousserHaut", "pousserBas", "pousserGauche", "pousserDroite"]:
+        return "B"
+    return None
+
 
 def sat_solver(infos):
     # a voir si on fusionne attaquerHaut et pousserHaut
@@ -43,12 +79,9 @@ def sat_solver(infos):
     at_least_one_action = [[var2n[('do', t, a)] for a in Actions] for t in range(t_max)]
     at_most_one_action = [[-var2n[('do', t, a1)], -var2n[('do', t, a2)]]
                           for a1, a2 in combinations(Actions, 2) for t in range(t_max)]
-    # on crée les clauses logique des positions de départ
 
-    mapdepart = []
-    for entite in map.keys():
-        for case in map[entite]:
-            mapdepart.append(var2n[('at', 0, case, entite)])
+    # on crée les clauses logique des positions de départ
+    mapdepart = [var2n[('at', 0, case, entite)] for entite in map.keys() for case in map[entite]]
 
     # goal
 
@@ -73,7 +106,7 @@ def sat_solver(infos):
     ]
     # deplacement d'un demon vers le haut
     deplacement_ennemi_haut = [
-        [var2n[('do', t, "attaquerHaut")], var2n[('at', t, c1, "H")], var2n[('at', t, (c1[0], c1[1] + 1), "D")],
+        [var2n[('do', t, "attaquerHaut")], var2n[('at', t, c1, "H")], var2n[('at', t, (c1[0], c1[1] + 1), "M")],
          var2n[('at', t + 1, (c1[0], c1[1] + 2), " ")],
          var2n[('at', t + 1, (c1[0], c1[1] + 2), "B")]]
         for t in range(t_max)
@@ -84,7 +117,7 @@ def sat_solver(infos):
 
     # eliminer demon haut
     eliminer_ennemi_haut = [
-        [var2n[('do', t, "tuerHaut")], var2n[('at', t, c1, "H")], var2n[('at', t, (c1[0], c1[1] + 1), "D")]]
+        [var2n[('do', t, "tuerHaut")], var2n[('at', t, c1, "H")], var2n[('at', t, (c1[0], c1[1] + 1), "M")]]
         for t in range(t_max)
         for c1 in Cases
         if (c1[0], c1[1] + 2) not in Cases
@@ -92,14 +125,35 @@ def sat_solver(infos):
     ]
 
     # mise a jour de la carte pour les element inchangés à un nouveau t
-    new_map = [[var2n('do', t, a), var2n('at', t + 1, c, entite), []] for t in range(t_max + 1) for a in Actions for c
-               in Cases for entite in map.keys()
-               if entite != consequence(a, c)]
 
-    Clauses = mapdepart + at_least_one_action + at_most_one_action + deplacement_personnage_Haut + deplacement_pierre_haut + deplacement_ennemi_haut + eliminer_ennemi_haut + new_map
+    # on fait reappaaraitre en t+1 toutes les entités sans rapport avec l'action
+    new_map = [[var2n[('at', t, c, entite)], var2n[('at', t + 1, c, entite)], var2n[('do', t, action)]]
+               for t in range(t_max)
+               for c in Cases
+               for entite in map.keys()
+               for action in Actions
+               if entite != get_target(action)
+               ]
+
+    # on fait reaparaiter les entirés avec un rapport avec l'action mais qui n'en sont pas la cible (par ex les mob non ciblés)
+    new_map += [[var2n[('at', t, c, entite)], var2n[('at', t + 1, c, entite)], var2n[('do', t, action)]]
+                for t in range(t_max)
+                for c in Cases
+                for entite in map.keys()
+                for action in Actions
+                if entite == get_target(action)
+                if action not in ["pousserHaut", "pousserBas", "pousserGauche", "pousserDroite", "attaquerHaut",
+                                  "attaquerBas", "attaquerGauche", "attaquerDroite"]
+                ]
+
+    goal = []
+
+    Clauses = mapdepart + at_least_one_action + at_most_one_action + deplacement_personnage_Haut + deplacement_pierre_haut + deplacement_ennemi_haut + eliminer_ennemi_haut + new_map + goal
+
+    return Clauses
 
 
 filename = r"D:\Alex\Etude\Superieur\UTC\Informatique\IA02\Projet\helltaker_ia02\levels\level1.txt"
 # récupération de la grille et de toutes les infos
 infos = grid_from_file(filename)
-sat_solver(infos)
+print(creation_cnf(sat_solver(infos)))
