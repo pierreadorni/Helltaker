07e786bc730cbf0bde5a8d7ae7e2ddde5bc870d7
Robin <robin.dereux@etu.utc.fr>
2022-06-08 12:23:46 +0200

    maj pas stable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from utils import grid_from_file
 from state_space_search.solver import *
-from satplan.SatSolver import creation_cnf, sat_solver
+from satplan.SatSolver import creation_cnf, sat_solver, exam
 from pprint import pprint
+import subprocess
 
 
 def state_space_search():
@@ -15,8 +16,16 @@ def state_space_search():
 def satplan():
     """Test for satplan"""
     infos = grid_from_file("maps/tests/corridor.txt")
-    cnf = creation_cnf(sat_solver(infos))
-    print(cnf)
+    sat = sat_solver(infos)
+    cnf = creation_cnf(sat)
+    # je vais mettre ca dans le satsolver
+    with open('clauses.cnf', 'w') as f:
+        f.write(cnf)
+    #print(cnf)
+    result = subprocess.run(["gophersat", "clauses.cnf"], capture_output=True).stdout.decode("utf-8")
+    with open('solve.cnf', 'w') as f:
+        f.write(result)
+    exam('clauses.cnf', 'solve.cnf', sat[2])
 
 
 if __name__ == "__main__":
