76027df58ae58cefc2e0110202a1901cf2739052
Alexandre Laval <alexandre.laval@etu.utc.fr>
2022-06-12 13:30:00 +0200

    ajout demon correction goal

--- satplan/SatSolver2.py
+++ satplan/SatSolver2.py
@@ -12,10 +12,10 @@ def creationLaby(infos):
             target = infos["grid"][i][j]
             map["board"].append((j, i))
             if target == "D":  # on ne représente pas les murs
-                map["final"].append(((j + 1, i)))
-                map["final"].append(((j - 1, i)))
-                map["final"].append(((j, i + 1)))
-                map["final"].append(((j, i - 1)))
+                map["final"].append((j + 1, i))
+                map["final"].append((j - 1, i))
+                map["final"].append((j, i + 1))
+                map["final"].append((j, i - 1))
                 map["demon"].append((j, i))
             elif target == "#":
                 map["wall"].append((j, i))
@@ -28,39 +28,7 @@ def creationLaby(infos):
     return map
 
 
-myLaby = {
-    "walls": (1, 1),
-    "board": {
-        (3, 4),
-        (3, 1),
-        (3, 7),
-        (2, 2),
-        (1, 6),
-        (2, 5),
-        (1, 3),
-        (2, 8),
-        (3, 3),
-        (3, 6),
-        (2, 4),
-        (1, 2),
-        (2, 1),
-        (2, 7),
-        (1, 5),
-        (1, 8),
-        (3, 2),
-        (3, 5),
-        (3, 8),
-        (1, 1),
-        (1, 4),
-        (2, 3),
-        (1, 7),
-        (2, 6),
-    },
-    "boxs": (),
-    "mobs": (),
-    "initial": (2, 3),
-    "final": (2, 6),
-}
+
 
 
 # In[2]:
@@ -75,11 +43,11 @@ def vocabulary(laby, t_max):
     wall_vars = [("wall", t, c) for t in range(t_max + 1) for c in laby["board"]]
     box_vars = [("box", t, c) for t in range(t_max + 1) for c in laby["board"]]
     mob_vars = [("mob", t, c) for t in range(t_max + 1) for c in laby["board"]]
+    demon_vars = [("demon", t, c) for t in range(t_max + 1) for c in laby["board"]]
 
-    return {v: i + 1 for i, v in enumerate(verb_vars + dir_vars + at_vars + wall_vars + box_vars + mob_vars)}
+    return {v: i + 1 for i, v in enumerate(verb_vars + dir_vars + at_vars + wall_vars + box_vars + mob_vars + demon_vars)}
 
 
-vocabulary(myLaby, 1)
 
 # In[3]:
 
@@ -107,7 +75,6 @@ def clauses_exactly_one_action(var2n, t_max):
     return at_least_one_dir + at_most_one_dir + at_least_one_verb + at_most_one_verb
 
 
-clauses_exactly_one_action(vocabulary(myLaby, 1), 1)
 
 
 # In[4]:
@@ -135,10 +102,13 @@ def clauses_initial_state(var2n, laby):
             cl.append([var2n[("mob", 0, c)]])
         else:
             cl.append([-var2n[("mob", 0, c)]])
+    for c in laby["board"]:
+        if c in laby["demon"]:
+            cl.append([var2n[("demon", 0, c)]])
+        else:
+            cl.append([-var2n[("demon", 0, c)]])
     return cl
 
-
-clauses_initial_state(vocabulary(myLaby, 1), myLaby)
 
 
 # In[6]:
@@ -163,13 +133,19 @@ def clauses_successor_from_given_position(var2n, laby, t_max, position):
         for t in range(t_max)
     ]
 
+    # les demons ne bougent pas
+    cl = [
+        [var2n[("demon", t, position)], -var2n[("demon", t + 1, position)]]
+        for t in range(t_max)
+    ]
+
     # les boîtes n'apparaissent pas
     cl += [
         [var2n[("box", t, position)], -var2n[("box", t + 1, position)]]
         for t in range(t_max)
     ]
 
-    # les monstress n'apparaissent pas
+    # les monstres n'apparaissent pas
     cl += [
         [var2n[("mob", t, position)], -var2n[("mob", t + 1, position)]]
         for t in range(t_max)
@@ -204,6 +180,15 @@ def clauses_successor_from_given_position(var2n, laby, t_max, position):
     # les murs non adjacents à la position ne disparaissent pas
     cl += [
         [-var2n[("at", t, position)], -var2n[("wall", t, c)], var2n[("wall", t + 1, c)]]
+        for t in range(t_max)
+        for c in board
+        if not (c in Successors.values())
+        if c != position
+    ]
+    # les demons non adjacents à la position ne disparaissent pas
+
+    cl += [
+        [-var2n[("at", t, position)], -var2n[("demon", t, c)], var2n[("demon", t + 1, c)]]
         for t in range(t_max)
         for c in board
         if not (c in Successors.values())
@@ -322,6 +307,17 @@ def clauses_successor_from_given_position(var2n, laby, t_max, position):
                     -var2n[("mob", t + 1, c)],
                 ]
                 for t in range(t_max)
+
+            ]
+            # on ne fonce pas dans les demons
+            cl += [
+                [
+                    -var2n[("at", t, position)],
+                    -var2n[("do_v", t, "move")],
+                    -var2n[("do_d", t, a)],
+                    -var2n[("demon", t + 1, c)],
+                ]
+                for t in range(t_max)
             ]
             # unicité de la position à l'issue de l'action
             cl += [
@@ -424,7 +420,6 @@ def clauses_successor_from_given_position(var2n, laby, t_max, position):
     return cl
 
 
-clauses_successor_from_given_position(vocabulary(myLaby, 1), myLaby, 1, (3, 1))
 
 
 # In[7]:
@@ -437,12 +432,16 @@ def sat_laby2(laby, t_max):
     )
     for c in laby["board"]:
         clauses += clauses_successor_from_given_position(var2n, laby, t_max, c)
-    clauses.append([var2n[("at", t_max, laby["final"])]])
+
+    if type(laby["final"]) == list:
+        for c in laby["final"]:
+            clauses.append([var2n[("at", t_max, c)]])
+    else:
+        clauses.append([var2n[("at", t_max, laby["final"])]])
+
     return var2n, clauses
 
 
-v2n, cl = sat_laby2(myLaby, 6)
-print(len(v2n), len(cl))
 
 # In[9]:
 
@@ -485,7 +484,8 @@ def exec_gophersat(filename: str, cmd: str = "gophersat", encoding: str = "utf8"
 def solve_laby2(infos):
     t_max = infos["max_steps"]
     laby = creationLaby(infos)
-    print(laby["board"])
+    print(laby)
+
     for t in range(1, t_max):
         v2n, cl = sat_laby2(laby, t)
         n2v = {i: v for v, i in v2n.items()}
@@ -494,9 +494,10 @@ def solve_laby2(infos):
         write_dimacs_file(dimacs, filename)
         sat, model = exec_gophersat(filename)
         if sat:
-            return [n2v[i] for i in model if i > 0 and n2v[i][0] in ("do_d", "do_v")]
+            print( [n2v[i] for i in model if i > 0 and n2v[i][0] in ("do_d", "do_v")])
         else:
             print("pas de plan de taille", t)
+
 
 
 # In[ ]:
